b81d2bc37df9b09ea08db30b943da95fa57ad2e5
Josh McEwen <josh.mcewen@and.digital>
2022-05-20 14:56:33 +0100

    Create shift function, unit tests and workflow file

--- main.go
+++ main.go
@@ -4,27 +4,41 @@ import (
 	"bytes"
 	"fmt"
 	"os"
+	"time"
 )
 
 func main() {
-	data, err := os.ReadFile("prideAndPrejudice.txt")
+	filename := "prideAndPrejudice"
+
+	start := time.Now()
+	data, err := os.ReadFile(fmt.Sprintf("%s.txt", filename))
 
 	if err != nil {
 		panic(err)
 	}
 
+	buffer := shift(data)
+	err = os.WriteFile(fmt.Sprintf("%s.txt", shift([]byte(filename))), buffer.Bytes(), 0644)
+	if err != nil {
+		panic(err)
+	}
+	elapsed := time.Since(start)
+	fmt.Printf("Execution: %s\n", elapsed)
+}
+
+func shift(b []byte) *bytes.Buffer {
 	buffer := bytes.NewBuffer([]byte{})
 
-	for _, b := range data {
-		isUppercase := b > 'A' && b < 'Z'
-		isLowercase := b > 'a' && b < 'z'
+	for _, b := range b {
+		isUppercase := b >= 'A' && b <= 'Z'
+		isLowercase := b >= 'a' && b <= 'z'
 
 		next := b
 
 		if isUppercase || isLowercase {
-			var z byte = 'Z'
-			if isLowercase {
-				z = 'z'
+			var z byte = 'z'
+			if isUppercase {
+				z = 'Z'
 			}
 
 			next = b + 2
@@ -35,6 +49,5 @@ func main() {
 		}
 		buffer.WriteByte(next)
 	}
-
-	fmt.Println(buffer.String())
+	return buffer
 }
---
+++ main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestShift(t *testing.T) {
+	buffer := shift([]byte("ABZ abz 0!"))
+
+	if buffer.String() != "CDB cdb 0!" {
+		t.Errorf("Unexpected result: %s", buffer.String())
+	}
+}
